ff320ee1fa95a06f41de8b852894353635a54e45
gimangi <khk211113@naver.com>
2022-05-02 22:21:46 +0900

    Fix unistep thread is never ending

--- pintos_qemu/projects/crossroads/unistep.c
+++ pintos_qemu/projects/crossroads/unistep.c
@@ -11,21 +11,40 @@ static void release_move() {
     }
 }
 
+static int is_finished() {
+    int i;
+    int ret = vi_cnt;
+    for (i=0; i<vi_cnt; i++) {
+        if (vi_list[i] == NULL || vi_list[i]->state == VEHICLE_STATUS_FINISHED)
+            ret--;
+    }
+    return ret;
+}
+
 void check_unistep() {
     int i;
+    char flag;
 
     while (1) {
 
+        if (is_finished())
+            break;
+
+        flag = 1;
+
         for (i=0; i<vi_cnt; i++) {
-        // Vehicle that have not yet moved 
-            if (vi_list[i] != NULL && vi_list[i]->state != VEHICLE_STATUS_FINISHED && vi_list[i]->moved.value == 1)
-                return;
+            // Vehicle that have not yet moved 
+            if (vi_list[i] != NULL && vi_list[i]->state != VEHICLE_STATUS_FINISHED && vi_list[i]->moved.value == 1) {
+                flag = 0;
+            }
+        }
+        // increase unistep
+        if (flag) {
+            release_move();
+
+            unitstep_changed();
+            crossroads_step++;
         }
 
-        release_move();
-
-        unitstep_changed();
-        crossroads_step++;
     }
-    
 }
