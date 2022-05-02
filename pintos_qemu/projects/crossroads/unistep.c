38700ef84fdecb9578cfda2a47e51aff38f01db5
gimangi <khk211113@naver.com>
2022-05-03 02:36:39 +0900

    lock unitstep

--- pintos_qemu/projects/crossroads/unistep.c
+++ pintos_qemu/projects/crossroads/unistep.c
@@ -29,6 +29,10 @@ void check_unitstep() {
     char flag;
 
     while (1) {
+        struct lock step_lock;
+        lock_init(&step_lock);
+        lock_acquire(&step_lock);
+
         flag = 1;
 
         for (i=0; i<vi_cnt; i++) {
@@ -45,9 +49,11 @@ void check_unitstep() {
             crossroads_step++;
 
             if (sem_released.value == 0) { 
+                lock_release(&step_lock);
                 break;
             }
         }
 
+        lock_release(&step_lock);
     }
 }
